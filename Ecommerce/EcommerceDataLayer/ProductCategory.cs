3383180cbb3b0a3ff6e4abb618d5069144365dc2
Nirnay <knirnay@gmail.com>
2016-07-18 13:32:08 -0700

    Adding comments to the product categroy entity

--- Ecommerce/EcommerceDataLayer/ProductCategory.cs
+++ Ecommerce/EcommerceDataLayer/ProductCategory.cs
@@ -2,8 +2,20 @@
 {
     public class ProductCategory
     {
+        /// <summary>
+        /// Gets or sets the category identifier.
+        /// </summary>
+        /// <value>
+        /// The category identifier.
+        /// </value>
         public int CategoryId { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
         public string Name { get; set; }
     }
 }
