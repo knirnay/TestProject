07b10d9171819be17f6e45390cb45336fb74b06b
knirnay <knirnay@gmail.com>
2016-08-09 23:46:27 -0700

    Update SpecificationProductCategoryXref.sql

--- Ecommerce/Ecommerce/SchemaObject/Tables/SpecificationProductCategoryXref.sql
+++ Ecommerce/Ecommerce/SchemaObject/Tables/SpecificationProductCategoryXref.sql
@@ -2,7 +2,7 @@
 (
 	SpecId INT NOT NULL,
 	CategoryId	INT NOT NULL,
-	InsertDate DATETIME CONSTRAINT [DF_InsertDate_SpecificationProductCategoryXref] DEFAULT (GETDATE()),
+	InsertDate DATETIME CONSTRAINT [DF_InsertDate_SpecificationProductCategoryXref] DEFAULT (GETDATE()) NOT NULL,
 	UpdateDate DATETIME NULL,
 	CONSTRAINT [PK_SpecificationProductCategoryXref_SpecId_CategoryId] PRIMARY KEY CLUSTERED (SpecId ASC, CategoryId ASC),
 	CONSTRAINT [FK_SpecificationProductCategoryXref_Specification_SpecId] FOREIGN KEY (SpecId) REFERENCES dbo.Specification(SpecId) ON UPDATE CASCADE ON DELETE CASCADE,
